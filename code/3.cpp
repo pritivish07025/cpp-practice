3cf1323e9fc8a0e8d2c94a2060775c74e0aabfa3
Priti Vishwakarma <priti.vishwakarma311664@gmail.com>
2025-11-25 00:40:45 +0530

    commit

---
+++ code/3.cpp
@@ -0,0 +1,12 @@
+//Add two numbers entered by the user.
+
+#include <iostream>
+using namespace std;
+int main(){
+int a, b;
+cout<<"enter two number:"<< endl;
+cin>> a>> b ;
+int sum=a+b;
+cout<< "sum of both the number : "<< sum<<endl;
+return 0;
+}
