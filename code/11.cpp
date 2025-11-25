02f484831888630d7e635bfd9590c9357ac12e6e
Priti Vishwakarma <priti.vishwakarma311664@gmail.com>
2025-11-25 15:35:31 +0530

    commit

---
+++ code/11.cpp
@@ -0,0 +1,21 @@
+//Find the largest among three numbers.
+
+#include <iostream>
+using namespace std;
+int main(){
+int a, b, c;
+cout<<"enter all the numbers:"<<endl;
+cin>>a;
+cin>>b;
+cin>>c;
+if(a>b&&a>c){
+cout<<" a is larger"<<endl;
+}
+else if(b>a &&b>> c){
+cout<<"b is larger "<< endl;
+}
+else {
+cout<<"c is larger "<<endl;
+}
+return 0;
+}
