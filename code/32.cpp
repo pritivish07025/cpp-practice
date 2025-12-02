2c2c9188ea9553b713bde97af995f6605e2fe313
Priti Vishwakarma <priti.vishwakarma311664@gmail.com>
2025-12-02 22:52:51 +0530

    commit

---
+++ code/32.cpp
@@ -0,0 +1,18 @@
+// sum of two numbers using functions
+
+#include <iostream>
+using namespace std;
+int sum(int a , int b){
+ int sum=a+b;
+cout<<sum;
+return 0;
+}
+
+int main(){
+int num1, num2;
+cout<<"enter both the number "<<endl;
+cin>>num1;
+cin>>num2;
+sum(num1, num2);
+return 0;
+}
